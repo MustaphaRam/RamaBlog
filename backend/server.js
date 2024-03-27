09627f33c9e1d5e6804fdc5ee171a8018b3f19da
MustaphaRam <mustapha.mad.75@gmail.com>
2024-03-27 22:07:18 +0000

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,80 @@
 // server.js
 const express = require('express');
-const db = require('./db');
+const db = require('./db.js');
+const userRoute = require('./routes/users.js');
+const authRotes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+
 const app = express();
 
-// check connection
-db.con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+// that is used to get request like format json 
+app.use(express.json());
+
+// cookieParser is a middleware, that is used to parse and convert 
+// cookies sent from the browser into a digestible JavaScript object.
+app.use(cookieParser());
+
+// upload image in server
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../frontend/public/upload')
+  },
+  filename: function (req, file, cb) {
+    // Specify the filename for uploaded files
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+})
+
+const upload = multer({ storage: storage })
+// send name file
+app.post('/api/upload', upload.single('file'), function (req, res, next) {
+  const file = req.file;
+  console.log("file has ben save : "+file.filename);
+  res.status(200).json(file.filename);
 });
 
-app.get('/', (req, res) => {
-  res.json('Hello In Rama Blog!');
+/* // check connection
+db.connect(function(err) {
+  if (err) {
+    console.error('Error connecting to MySQL database: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to MySQL database as id ' + db.threadId);
+  
+  // Close the connection after checking
+  db.end(function(err) {
+    if (err) {
+      console.error('Error closing MySQL connection: ' + err.stack);
+      return;
+    }
+    console.log('MySQL connection closed.');
+  });
+}); */
+
+
+// routes post
+// localhost:8800/api/posts/route
+app.use("/api/posts/", postRoutes);
+
+// routes auth
+app.use("/api/auth/", authRotes);
+
+// routes user
+app.use("/api/user/", userRoute);
+
+
+
+
+
+// first route
+app.get('/api', (req, res) => {
+  res.json('Welcome To Rama Blog!');
 });
 
-const port = 3800;
-app.listen(8300, () => {
-  console.log(`app listening at http://localhost:${port}`);
+// run server
+app.listen(8800, () => {
+  console.log(`app listening at http://localhost:8800/`);
 });
