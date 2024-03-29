48cba68f153618fa61c2af456c34f742232a9842
MustaphaRam <mustapha.mad.75@gmail.com>
2024-03-29 16:23:08 +0000

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../context/authContext";
+import { AuthContext } from "../auth/authContext";
 import Logo from "../img/logo.png";
 
 const Navbar = () => {
