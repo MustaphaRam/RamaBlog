08ff7c85e8342eefaae0d28a353b1ef295fdfbd5
MustaphaRam <mustapha.mad.75@gmail.com>
2024-03-30 01:31:50 +0000

    login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/authContext";
+import Spinner from '../components/Spinner';
+
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
+  var [spinner, setspinner] = useState(false);
+
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setspinner(true);
+
+    try {
+      await login(inputs)
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      if(err.response.status === 400) {
+        setError(err.response.data);
+      };
+      if (err.response.status === 409 || err.response.status === 404){
+        setError(err.response.data);
+      }
+    }
+
+    setspinner(false);
+  };
+  return (
+    <div className='auth'>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} className='col-md-6'>
+        <input type="username" name='username' className="form-control" onChange={handleChange} placeholder="username" required />
+        <input type="password" name='password' className="form-control" onChange={handleChange} placeholder="Password" autoComplete="true" required />
+        <button type="submit" disabled={spinner}>Login
+          {spinner && <Spinner />}
+        </button>
+        {err && <p>{err}</p>}
+        <span>
+          Don't you have an account? <Link to="/register">Signup</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
