98776a19a1d1e1b35286ef99fd5a42654e97bfbe
MustaphaRam <mustapha.mad.75@gmail.com>
2024-04-03 14:12:24 +0000

    routes user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { profile, changeImg, changePsaword } = require("../controllers/user");
+const { check } = require('express-validator');
 
 const router = express.Router();
 
+
+router.get('/:id/profile', profile);
+router.put('/:id/changepasword', [check('newPassword').isLength({ min: 6, max: 30 }).withMessage('Password must be between 6 and 20 characters')], changePsaword)
+router.put('/:id/profile/image', changeImg)
 
 module.exports = router;
